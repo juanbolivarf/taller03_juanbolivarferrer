690d502d0d0007715c35e2e52a9c9ff580114b8e
juanbolivar <juanbolivarf20@gmail.com>
2024-09-23 09:53:31 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import myLogo from './assets/fedwb2012.png'
+
+function App() { 
+  return (
+    <>
+      <div>
+        <img src={myLogo} alt="Federer logo" />
+        <h1> Roger Federer </h1>
+        <p> Roger Federer es uno de esos escasos atletas sobrenaturales que parecen estar exentos, por lo menos en parte, de ciertas leyes de la física. Otros seres comparables serían Michael Jordan, que no solo podía dar saltos inhumanamente altos, sino también quedarse suspendido en el aire un momento o dos más de los que permitía la gravedad, y Muhammad Ali, que realmente podía cruzar «flotando» la lona y asestar dos o tres golpes cortos en el tiempo necesario para dar uno.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
